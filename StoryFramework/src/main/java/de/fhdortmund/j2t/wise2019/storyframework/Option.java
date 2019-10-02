bddf329ad25cddee990927b4d94905309623a7e3
Tim Ittermann <tim.ittermann@gmail.com>
2019-10-02 10:08:48 +0200

    Vererbung beachtet

--- StoryFramework/src/main/java/de/fhdortmund/j2t/wise2019/storyframework/Option.java
+++ StoryFramework/src/main/java/de/fhdortmund/j2t/wise2019/storyframework/Option.java
@@ -19,7 +19,7 @@ public class Option {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || !(o instanceof Option)) return false;
         Option option = (Option) o;
         return id == option.id;
     }
