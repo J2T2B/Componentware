0e51225381c8cfb78767f041f52185659bc385be
Tim Ittermann <tim.ittermann@gmail.com>
2019-10-09 11:15:35 +0200

    Hello World mit Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Jumbotron } from 'reactstrap';
 
-// Idee: https://bootsnipp.com/snippets/exR5v
+export class App extends React.Component<{}, {}> {
+    
+    render() {
+        return <Container>
+            <Jumbotron>
+                <h1 className="display-3">
+                    Hallo Welt :)
+                </h1>
+            </Jumbotron>
+        </Container>;
+    }
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
-export default App;
+// Idee: https://bootsnipp.com/snippets/exR5v
