0b388dbfde9ff0e8750ac8bbfc376c633037dbc8
Tim Ittermann <tim.ittermann@gmail.com>
2019-11-13 10:59:28 +0100

    Ausblendbare Chatliste

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { MockComponent } from './components/MockComponent';
-import { Container, Col, Row, Navbar, NavbarBrand } from 'reactstrap';
+import { Container, Col, Row, Navbar, NavbarBrand, Nav, NavItem, NavLink, Button } from 'reactstrap';
 import { Router, Switch, Route } from 'react-router';
 import { HashRouter } from 'react-router-dom';
 import { ChatsListComponent } from './components/ChatListComponent';
 import AChatsHandler from './logic/AChatsHandler';
 import { MockChatsHandler } from './logic/MockChatsHandler';
-import {ChatMessageComponent} from "./components/ChatMessageComponent";
+import { ChatMessageComponent } from "./components/ChatMessageComponent";
 
-export class App extends React.Component<{}, {}> {
+interface AppStates {
+    isChatListOpen: boolean;
+}
+
+export class App extends React.Component<{}, AppStates> {
 
     private chatsHandler: AChatsHandler;
 
@@ -17,6 +21,18 @@ export class App extends React.Component<{}, {}> {
         super(props);
         this.chatsHandler = new MockChatsHandler();
         this.chatsHandler.connect();
+
+        this.state = {
+            isChatListOpen: true
+        };
+    }
+
+    toggleChatsList() {
+        this.setState(o => {
+            return {
+                isChatListOpen: !o.isChatListOpen
+            }
+        })
     }
 
     render() {
@@ -27,12 +43,19 @@ export class App extends React.Component<{}, {}> {
                 <NavbarBrand href="/" className="mr-auto">
                     Telekilo
                 </NavbarBrand>
+                <Nav>
+                    <NavItem className="d-md-none">
+                        <Button color="primary" onClick={this.toggleChatsList.bind(this)}>
+                            Chatliste
+                        </Button>
+                    </NavItem>
+                </Nav>
             </Navbar>
 
             <Container fluid>
                 <Row>
                     <Col md={4}>
-                        <ChatsListComponent chatsHandler={this.chatsHandler} />
+                        <ChatsListComponent chatsHandler={this.chatsHandler} isOpen={this.state.isChatListOpen} />
                     </Col>
                     <Col md={8}>
                         <HashRouter>
