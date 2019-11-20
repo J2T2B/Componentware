efdf17be33db8ea32e9cde9ff49c4ad68ec1cdc0
Tim Ittermann <tim.ittermann@gmail.com>
2019-11-20 10:29:21 +0100

    Deutsches Moment

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/main.scss';
 import {App} from './App';
 import * as serviceWorker from './serviceWorker';
+import "moment/locale/de";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
