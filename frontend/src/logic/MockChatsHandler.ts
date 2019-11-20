78d1a6beb4a1b48ff7441b18608776a60eb88377
Tim Ittermann <tim.ittermann@gmail.com>
2019-11-20 10:28:32 +0100

    Mocking

--- frontend/src/logic/MockChatsHandler.ts
+++ frontend/src/logic/MockChatsHandler.ts
@@ -7,9 +7,12 @@ export class MockChatsHandler extends AChatsHandler {
     public async connect(): Promise<boolean> {
         (window as any).simulateMessage = this.simulateMessage.bind(this);
         console.log("Globale Funktion simulateMessage(...) hinzugefügt");
+
+        window.setTimeout(this.quereMessage.bind(this), 2000);
+
         return true;
     }
-    
+
     protected sendMessage(socketMessage: SocketMessage): VoidLike {
         console.log("Nachricht an Server", socketMessage);
     }
@@ -21,6 +24,39 @@ export class MockChatsHandler extends AChatsHandler {
         else {
             this.onSocketMessage(message);
         }
+    }
+
+    private quereMessage() {
+        for (let i = 0; i < 5; i++) {
+            this.simulateMessage(
+                {
+                    command: "CreateChat",
+                    chat: {
+                        chatId: i,
+                        partner: {
+                            name: "Kunde " + (i + 1),
+                            imageUrl: "",
+                        },
+                        messages: [],
+                    }
+                });
+
+            for (let j = 0; j < 10; j++) {
+                this.simulateMessage({
+                    command: "AddMessage",
+                    chatId: i,
+                    message: {
+                        id: ((i*10)+j).toString(),
+                        text: "Hallo Will "+j,
+                        image: "",
+                        answers: [],
+                        userHasRead: false,
+                        created: (new Date().getTime() - 60000),
+                        isAnswer: false
+                    }
+                });
+            }
+        }        
     }
 
 }
