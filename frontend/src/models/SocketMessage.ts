d6226c8ab71d97042c7b24f1f7dd28f345de974f
Tim Ittermann <tim.ittermann@gmail.com>
2020-01-08 10:34:46 +0100

    Points Datenstruktur hinzugefügt

--- frontend/src/models/SocketMessage.ts
+++ frontend/src/models/SocketMessage.ts
@@ -1,31 +1,35 @@
 import { IChat } from "./Chat";
 import { IMessage } from "./Message";
 import { Answer } from "./Answer";
+import { Points } from "./Points";
 
 export type SocketMessage =
-{
-    command: "CreateChat"
-    chat: IChat
-} |
-{
-    command: "AddMessage"
-    chatId: number
-    message: IMessage
-} |
-{
-    command: "AddAnswer",
-    chatId: number,
-    messageId: string,
-    answer: Answer
-} | 
-{
-    command: "SubmitAnswer",
-    answerId: number
-} |
-{
-    command: "Reinit"
-} |
-{
-    command: "ReadMessage",
-    messageId: string
-}
+    {
+        command: "CreateChat"
+        chat: IChat
+    } |
+    {
+        command: "AddMessage"
+        chatId: number
+        message: IMessage
+    } |
+    {
+        command: "AddAnswer",
+        chatId: number,
+        messageId: string,
+        answer: Answer
+    } |
+    {
+        command: "SubmitAnswer",
+        answerId: number
+    } |
+    {
+        command: "Reinit"
+    } |
+    {
+        command: "ReadMessage",
+        messageId: string
+    } |
+    ({
+        command: "ChangePoints",
+    } & Points) // Enthält alle Felder aus Points 
