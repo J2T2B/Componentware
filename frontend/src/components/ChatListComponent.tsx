85451c80e3091408212cf9fc52b9d6f984befadc
Tim Ittermann <tim.ittermann@gmail.com>
2020-01-08 10:44:07 +0100

    Punkte werden angezeigt

--- frontend/src/components/ChatListComponent.tsx
+++ frontend/src/components/ChatListComponent.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Chat } from "../models/Chat";
-import {Card, CardBody, ListGroup, CardTitle, Container} from "reactstrap";
+import { Card, CardBody, ListGroup, Container, Progress } from "reactstrap";
 import { DefaultComponentProps } from "../DefaultComponentProps";
 import IChatsListener from "../logic/IChatsListener";
 import { VoidLike } from "../logic/VoidLike";
 import { ChatListItemComponent } from "./ChatListItemComponent";
 import AChatsHandler from "../logic/AChatsHandler";
+import { Points } from "../models/Points";
 
 export interface ChatListProps extends DefaultComponentProps {
     isOpen: boolean;
@@ -15,22 +16,33 @@ export interface ChatListProps extends DefaultComponentProps {
 export interface ChatListStates {
     chats: Chat[]
     currentChat?: Chat
+    currentPoints: Points;
 }
 
 export class ChatsListComponent extends React.Component<ChatListProps, ChatListStates> implements IChatsListener {
-    
+
+
     constructor(props: ChatListProps) {
         super(props);
         this.state = {
             chats: [],
-            currentChat: undefined
+            currentChat: undefined,
+            currentPoints: {
+                chefSatisfaction: 0,
+                customerExperience: 0,
+                budget: 0
+            }
         };
+    }
+
+    onPointsChange(currentPoints: Points): VoidLike {
+        this.setState({ currentPoints });
     }
 
     onCurrentChatChange(currentChat?: Chat): VoidLike {
         this.setState({ currentChat });
     }
-    
+
     onChatChange(chats: Chat[]): VoidLike {
         this.setState({ chats });
     }
@@ -44,8 +56,10 @@ export class ChatsListComponent extends React.Component<ChatListProps, ChatListS
     }
 
     render() {
-        
+
         if (!this.props.isOpen) return <></>;
+
+        const { chats, currentPoints } = this.state;
 
         return <Container>
             <Card color="secondary">
@@ -55,10 +69,24 @@ export class ChatsListComponent extends React.Component<ChatListProps, ChatListS
                         William Walker
                     </div>
                 </CardBody>
+                <CardBody>
+                    <div>
+                        Kundenerfahrungen
+                        <Progress value={currentPoints.customerExperience} />
+                    </div>
+                    <div>
+                        Chefzufriedenheit
+                        <Progress value={currentPoints.chefSatisfaction} />
+                    </div>
+                    <div>
+                        Firmenbudget
+                        <Progress value={currentPoints.budget} />
+                    </div>
+                </CardBody>
             </Card>
             <ListGroup>
                 {
-                    this.state.chats.map(c => <ChatListItemComponent chat={c} key={c.chatId} chatsHandler={this.props.chatsHandler} />)
+                    chats.map(c => <ChatListItemComponent chat={c} key={c.chatId} chatsHandler={this.props.chatsHandler} />)
                 }
             </ListGroup>
         </Container>;
