1362642a616a5b18b9919059e5f1b5b2594cc6aa
JReibner <preibner@hotmail.de>
2020-02-02 14:58:16 +0100

    CommandModels angepasst

--- server/src/main/java/de/fhdortmund/j2t2/wise2019/server/game/remote/websocketcommands/AddMessageWebSocketCommand.java
+++ server/src/main/java/de/fhdortmund/j2t2/wise2019/server/game/remote/websocketcommands/AddMessageWebSocketCommand.java
@@ -8,16 +8,16 @@ import de.fhdortmund.j2t2.wise2019.server.game.remote.models.MessageRemoteModel;
 public class AddMessageWebSocketCommand extends AbstractWebSocketCommand {
 
     private static final String COMMAND = "AddMessage";
-    private final long chatId;
+    private final String chatId;
     private final MessageRemoteModel message;
 
     public AddMessageWebSocketCommand(long chatId, Chat.ChatMessage message) {
         super(COMMAND);
-        this.chatId = chatId;
+        this.chatId = chatId+"";
         this.message = new MessageRemoteModel(message);
     }
 
-    public long getChatId() { return chatId; }
+    public long getChatId() { return Long.parseLong(chatId); }
 
     public MessageRemoteModel getMessage() {
         return message;
